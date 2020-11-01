9fe02b290b0174bb823d5db84bdfed499b2160d9
GermoEDLP <german.brassini@gmail.com>
2020-10-31 21:15:01 -0300

    Estructura de funcionamiento inicial

---
+++ src/share/TopBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <v-app-bar app elevate-on-scroll color="green" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Alco</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TopBar'
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div v-if="documents.length > 0">
+      {{documents}}
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import { db } from '@/db';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      documents: [],
+    }
+  },
+  created(){
+    db.collection('user')
+  .get()
+  .then(querySnapshot => {
+    this.documents = querySnapshot.docs.map(doc => doc.data())
+    // do something with documents
+  })
   }
 }
 </script>
